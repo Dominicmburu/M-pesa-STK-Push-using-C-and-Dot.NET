3d936cb8ef6c651d1bb7bbd91a09d594cdca5ec0
Dominic Mburu <dominicmburu034@gmail.com>
2024-05-24 17:14:37 +0300

    first commit

---
+++ mpesa/Home.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="mpesa.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+     <style>
+            /* Custom CSS for centering content */
+            .center-horizontal {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                min-height: 100vh;
+            }
+        </style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container mt-12">
+        <div class="row justify-content-center center-horizontal">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">M-Pesa Payment</h5>
+                        <div class="form-group">
+                            <label for="txtPhoneNumber">Phone Number:</label>
+                            <asp:TextBox ID="txtPhoneNumber" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="txtAmount">Amount:</label>
+                            <asp:TextBox ID="txtAmount" runat="server" CssClass="form-control" />
+                        </div>
+                        <div class="text-center">
+                            <asp:Button ID="btnPay" runat="server" Text="Pay using M-Pesa" CssClass="btn btn-primary" OnClick="btnPay_Click" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ mpesa/Home.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace mpesa
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPay_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = txtPhoneNumber.Text;
+            string amount = txtAmount.Text;
+            string token = GetOAuthToken();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                InitiateSTKPush(token, phoneNumber, amount);
+            }
+        }
+
+        private string GetOAuthToken()
+        {
+            string url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+            string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes("iNPXCUm9Fvst1Xvtlz5vOYzk68nMsCorGGO68ylnGRCiNTlF:3Sv6zM74lvz4LX5vhCh9XSdzplmF4J16BwnrWGjSn7vDr9KnZrSGU70mevsPY1lM"));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", "Basic " + credentials);
+            request.Method = "GET";
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string jsonResponse = reader.ReadToEnd();
+                    dynamic result = JsonConvert.DeserializeObject(jsonResponse);
+                    return result.access_token;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                System.Diagnostics.Debug.WriteLine("Error getting OAuth token: " + ex.Message);
+                return null;
+            }
+        }
+
+        private void InitiateSTKPush(string token, string phoneNumber, string amount)
+        {
+            string url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("Authorization", "Bearer " + token);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            string businessShortCode = "174379";
+            string lipaNaMpesaOnlinePasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string password = Convert.ToBase64String(Encoding.UTF8.GetBytes(businessShortCode + lipaNaMpesaOnlinePasskey + timestamp));
+
+            var stkPushRequest = new
+            {
+                BusinessShortCode = businessShortCode,
+                Password = password,
+                Timestamp = timestamp,
+                TransactionType = "CustomerPayBillOnline",
+                Amount = amount,
+                PartyA = phoneNumber,
+                PartyB = businessShortCode,
+                PhoneNumber = phoneNumber,
+                CallBackURL = "https://us-central1-norse-journey-412116.cloudfunctions.net/callbackHandler",
+                AccountReference = "Test123",
+                TransactionDesc = "Payment for test"
+            };
+
+            string json = JsonConvert.SerializeObject(stkPushRequest);
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string result = reader.ReadToEnd();
+                    // Handle response from M-Pesa
+                    System.Diagnostics.Debug.WriteLine("STK Push response: " + result);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                System.Diagnostics.Debug.WriteLine("Error initiating STK Push: " + ex.Message);
+            }
+        }
+    }
+}
